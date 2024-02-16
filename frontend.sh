65afe400929ee089e3a21164b3736dd7f16470ca
jeevan <jeev5324u@gmail.con>
2024-02-16 22:27:17 +0530

    01-f

--- frontend.sh
+++ frontend.sh
@@ -6,9 +6,9 @@ systemctl enable nginx
 systemctl start nginx
 
 
+rm -rf /usr/share/nginx/html/*
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
-rm -rf *
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 unzip /tmp/frontend.zip
 # note : we are switching the location to /usr/share/nginx/html. from this location it looks for the file so we need to comeback again
 
