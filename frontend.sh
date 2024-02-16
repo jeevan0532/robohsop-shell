741cbbbc14a9d2780ff8e4aed0b56903a963cfe5
jeevan <jeev5324u@gmail.con>
2024-02-16 22:58:58 +0530

    01-f

--- frontend.sh
+++ frontend.sh
@@ -12,6 +12,6 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 # note : we are switching the location to /usr/share/nginx/html. from this location it looks for the file so we need to comeback again
 
-cp $script_location/files/nginx/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp $script_location/files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 systemctl restart nginx
