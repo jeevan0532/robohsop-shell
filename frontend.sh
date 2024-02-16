c374b92dbc60e8083c0e9014d14fa841583d78aa
jeevan <jeev5324u@gmail.con>
2024-02-16 22:12:45 +0530

    MJK-01-frontend

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-script-location=${pwd}
+script_location=${pwd}
 
 yum install nginx -y
 systemctl start nginx
@@ -8,7 +8,7 @@ systemctl enable nginx
 
 cd /usr/share/nginx/html/*
 rm -rf *
-curl -o ${script-location}/tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o $script_location/tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
 unzip /tmp/frontenc.zip
 ## note : we are switching the location to /usr/share/nginx/html. from this location it looks for the file so we need to comeback again
 
