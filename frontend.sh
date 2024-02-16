d3ad31a9c5136c42c74d43461d1a0636612efdaa
jeevan <jeev5324u@gmail.con>
2024-02-16 22:16:07 +0530

    MJK-01-frontend

--- frontend.sh
+++ frontend.sh
@@ -9,7 +9,7 @@ systemctl enable nginx
 cd /usr/share/nginx/html/*
 rm -rf *
 curl -o $script_location/tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-unzip /tmp/frontenc.zip
+unzip /tmp/frontend.zip
 ## note : we are switching the location to /usr/share/nginx/html. from this location it looks for the file so we need to comeback again
 
 cp /files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf
