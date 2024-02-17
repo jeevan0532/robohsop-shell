f5cf83b76f275668b6a1480ea8bab18ba1685bc9
jeevan <jeev5324u@gmail.con>
2024-02-17 15:12:28 +0530

    MJK-02-mongodb-setup

---
+++ mongodb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+script_location=$(pwd)
+cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+
+#edit file in mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+
+systemctl restart mongod
