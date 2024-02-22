f9a636b0f6fecc3efc292e9aae8ee2711b225983
jeevan <jeev5324u@gmail.con>
2024-02-22 21:14:32 +0530

    frontend mod

--- frontend.sh
+++ frontend.sh
@@ -2,76 +2,42 @@
 script_location=$(pwd)
 log=/tmp/roboshop.log
 
+status_check() {
+if [ $? -eq 0 ]; then
+        echo -e "\e[31m success \e[0m"
+else
+        echo -e "\e[31m failure \e[0m"
+	echo "failure, refer to the log-${log}"
+	exit
+fi
+
 echo -e "\e[35minstall nginx\e[0m"
 yum install nginx -y &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-	echo -e "\e[31m success \e[0m"
-else
-	echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
 echo -e "\e[35mremove old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
-
+status_check
 
 echo -e "\e[35minstall forntend content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
 echo -e "\e[35munzip frontend content\e[0m"
 cd /usr/share/nginx/html &>>${log}
 unzip /tmp/frontend.zip &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
 # note : we are switching the location to /usr/share/nginx/html. from this location it looks for the file so we need to comeback again
 echo -e "\e[35mseting up nginx op conf file\e[0m"
 cp $script_location/files/nginx.roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
 echo -e "\e[35menable nginx\e[0m"
 systemctl enable nginx &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
 echo -e "\e[35mstart nginx\e[0m"
 systemctl restart nginx &>>${log}
-echo $?
-
-if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
-else
-        echo -e "\e[31m failure \e[0m"
-fi
+status_check
 
