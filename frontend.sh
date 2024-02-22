35225b024663cbf07c89dc0ec954885c3e93f341
jeevan <jeev5324u@gmail.con>
2024-02-22 21:19:59 +0530

    ll

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 script_location=$(pwd)
 log=/tmp/roboshop.log
+set -e
 
 status_check() {
 if [ $? -eq 0 ]; then
-        echo -e "\e[31m success \e[0m"
+        echo -e "\e[31m success\e[0m"
 else
-        echo -e "\e[31m failure \e[0m"
+        echo -e "\e[31m failure\e[0m"
 	echo "failure, refer to the log-${log}"
 	exit
 fi
