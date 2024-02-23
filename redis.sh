ad90fedc0892a5f1527126899427ce0605ff2f62
jeevan <jeev5324u@gmail.con>
2024-02-23 14:15:51 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,20 +1,25 @@
 source common.sh
+i
+print_head "configuring yum repos" &>>{log}
 
-print_head "configuring yum repos"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
-print_head "enable redis remi"
+print_head "enable redis remi" &>>{log}
+
 dnf module enable redis:remi-6.2 -y
 
-print_head "install redis"
+print_head "install redis" &>>{log}
+
 yum install redis -y
 
-print_head "modifying redis.conf"
-sed -i -e "/127.0.0.1/0.0.0.0/" /etc/redis.conf
+print_head "modifying redis.conf" &>>{log}
 
-print_head "enable redis"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf
+
+print_head "enable redis" &>>{log}
+
 systemctl enable redis
 
-print_head "start redis"
+print_head "start redis" &>>{log}
 systemctl start redis
 
