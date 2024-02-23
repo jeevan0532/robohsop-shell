6976b40b151f8ceec33ac9cff126bd857b1059f4
jeevan <jeev5324u@gmail.con>
2024-02-23 13:54:49 +0530

    mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,25 @@
-#!/bin/bash
+source common.sh
 
-script_location=$(pwd)
-cp $script_location/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+print_head "copying mongodb repo file"
+cp ${script_location}/app/mongodb.repo  /etc/yum.repos.d/mongo.repo
+status_check
 
+print_head "installing mongodb"
 yum install mongodb-org -y
+status_check
+
+print_head "enable mongod"
 systemctl enable mongod
+status_check
+
+print_head "start mongod"
 systemctl start mongod
+status_check
 
-# edit file in mongod.conf
-
+print_head "edit mongod.conf file"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+status_check
 
-
+print_head "restart mongod"
 systemctl restart mongod
+status_check
