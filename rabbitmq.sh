777c9b47bf0f7890f0942afb7b72e578b30fa258
jeevan <jeev5324u@gmail.con>
2024-02-24 20:54:09 +0530

    rabbitmq setup

---
+++ rabbitmq.sh
@@ -0,0 +1,31 @@
+source common.sh
+
+if [ -z "s${rabbitmq_pass}" ]; then
+	echo "rabbitmq pass is missing"
+fi
+
+print_head "yum repos for erlang"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+status_check
+
+print_head "yum repos for rabbitmq"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+status_check
+
+
+print_head "install rabbitmq & erlang"
+yum install rabbitmq-server erlang -y
+status_check
+
+
+print_head "enable and start"
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server 
+status_check
+
+
+print_head "add user and password"
+rabbitmqctl add_user roboshop ${rabbitmq_pass}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+status_check
+
