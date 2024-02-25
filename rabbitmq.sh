2de72ca5a00736218d1453afe0f9daee9b4496e8
jeevan <jeev5324u@gmail.con>
2024-02-25 22:06:20 +0530

    rabbitmq update-1

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -5,30 +5,35 @@ if [ -z "${rabbitmq_pass}" ]; then
 fi
 
 print_head "yum repos for erlang"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log
 status_check
 
 print_head "yum repos for rabbitmq"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log
 status_check
 
 
 print_head "install rabbitmq & erlang"
-yum install rabbitmq-server erlang -y
+yum install rabbitmq-server erlang -y  &>>$log
+
 status_check
 
 
 print_head "enable and start"
-systemctl enable rabbitmq-server 
-systemctl start rabbitmq-server 
+systemctl enable rabbitmq-server  &>>$log
+
+systemctl start rabbitmq-server  &>>$log
+
 status_check
 
 
 print_head "add user and password"
-rabbitmqctl list_users | grep roboshop
+rabbitmqctl list_users | grep roboshop &>>$log
+
 if [ $? -ne 0 ]; then
  rabbitmqctl add_user roboshop ${rabbitmq_pass}
 fi
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>$log
+
 status_check
 
